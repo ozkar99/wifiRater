702c890d683fd038b2cc0b360b32f923da20e63f
Oscar Moreno Garza <oscarmg99@gmail.com>
2014-03-02 20:03:40 -0600

    new file:   README.txt 	modified:   TODO.txt 95% Completado el servicio.

---
+++ Service/README.txt
@@ -0,0 +1,10 @@
+Este servicio es una API REST.
+
+
+Explicacion de llamadas:
+
+/Ratings/ -> Te regresa una lista de pares, [bssid, rating]
+
+/Ratings/:bssid  -> Te regresa 1 solo par [bssid, rating]
+
+/Ratings/:bssid/:rating -> Actualiza (o inserta si no existe) en la base de datos, luego redirige a /Ratings/:bssid
--- Service/wifiRaterService.rb
+++ Service/wifiRaterService.rb
@@ -4,12 +4,76 @@ require 'sinatra'
 require 'json'
 require 'mysql'
 
+user='furby'
+pass='furby'
+host='localhost'
+database = 'wifiRater'
 
-get '/' do
-	#todo el ratings table
+
+con = Mysql.new host, user, pass, database
+
+
+
+######################## Auth ##########################
+use Rack::Auth::Basic, "Restricted Area" do |username, password|
+  username == user and password == pass
 end
 
-get '/:bssid' do
-    #se manda el bssid y rating 
-	"#{params[:bssid]}"
+################ Home (load Readme.txt) #######################
+get '/' do
+      page = File.read('README.txt')
+      page.gsub(/\n/, "</br>")
+end
+
+########################### Ratings #############################
+
+#send all the ratings
+get '/Ratings/?' do
+
+	#dem whole ratings
+    rs = con.query("SELECT * FROM Ratings")
+
+    #iterate over sets of data
+    rs.each do |col|
+            col.to_json()
+    end
+
+
+end
+
+#Send bssid and rating pair
+get '/Ratings/:bssid/?' do
+    rs = con.query("SELECT * FROM Ratings WHERE bssid='#{params[:bssid]}'")
+    rs.fetch_row().to_json()
 end 
+
+#Recieve new rating for bssid
+get '/Ratings/:bssid/:rating/?' do
+    
+    #check if record exist
+    rs = con.query("SELECT * FROM Ratings WHERE bssid='#{params[:bssid]}'")
+    
+    if rs.fetch_row().nil?
+        #if the bssid doesnt exist create it
+        con.query("INSERT INTO Ratings (rating, bssid) VALUES (#{params[:rating]}, '#{params[:bssid]}')")     
+   else
+        #else update it
+        con.query("UPDATE Ratings SET rating=#{params[:rating]} WHERE bssid='#{params[:bssid]}'")
+   end
+    
+    #redirect to the get
+    redirect to("/Ratings/#{params[:bssid]}")
+
+end
+
+
+################### User hashes:#########################
+get '/User/?' do
+    "Nigga, please... especify userame pls";
+end
+
+#Send user hashed passwd
+get '/User/:user/?' do
+    rs = con.query("SELECT passwd FROM Users WHERE user='#{params[:user]}'")
+    rs.fetch_row().to_json()
+end
