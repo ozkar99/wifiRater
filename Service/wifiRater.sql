a1f2ba842f026e40ce5f80d94945cd1b61930af2
Oscar Moreno Garza <oscarmg99@gmail.com>
2014-03-02 20:04:23 -0600

    new file:   wifiRater.sql 	Sample mysql database for wifiRater

---
+++ Service/wifiRater.sql
@@ -0,0 +1,77 @@
+-- MySQL dump 10.14  Distrib 5.5.33a-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: wifiRater
+-- ------------------------------------------------------
+-- Server version	5.5.33a-MariaDB-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Ratings`
+--
+
+DROP TABLE IF EXISTS `Ratings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Ratings` (
+  `bssid` char(17) NOT NULL,
+  `rating` int(11) DEFAULT NULL,
+  PRIMARY KEY (`bssid`),
+  UNIQUE KEY `bssid` (`bssid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Ratings`
+--
+
+LOCK TABLES `Ratings` WRITE;
+/*!40000 ALTER TABLE `Ratings` DISABLE KEYS */;
+INSERT INTO `Ratings` VALUES ('00:11:22:33:44:55',1),('00:1F:9F:A3:F5:D4',2),('00:23:69:91:FB:35',5);
+/*!40000 ALTER TABLE `Ratings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Users` (
+  `user` varchar(100) NOT NULL,
+  `passwd` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`user`),
+  UNIQUE KEY `user` (`user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES ('furby','*715D7B033E1993278946BB41B3B2BC507DF98D49'),('ozkar','*91D9861DFC07DD967611B8C96953474EF270AD5E');
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-03-02 20:04:15
